846dca284ff0d9cdc390f2d3f1d51d0205b5a9c2
osky104 <oscar.farina@buonarroti.tn.it>
2026-03-05 09:31:43 +0100

    inizio header

--- index.php
+++ index.php
@@ -1,16 +1,34 @@
-<?php
-    require 'takeData.php';
-    require 'database.php';
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Previsioni meteo</title>
+    <title>Document</title>
 </head>
 <body>
-    
+        <header>
+            <div>
+                <div>
+                    <h1 class="font-bold text-xl text-gray-900 dark:text-white">Meteo Trentino</h1>
+                    <p class="text-xs text-gray-600 dark:text-gray-400">Qualità dell'aria e previsioni</p>
+                </div>
+            
+                <nav>
+                    <a href=""><img src="" alt=""><button>Home</button></a>
+                    <a href="forecast.php"><button>Forecast</button></a>
+                    <a href="airQuality.php"><img src="" alt=""><button>Air Quality</button></a>
+                    <a href="historical.php"><button>Historical</button></a>
+                </nav>
+            </div>
+        </header>
+
+
+
+
+        
+        <footer>
+            <p>© 2026 Meteo Trentino - Dati meteo e qualità dell'aria per il Trentino-Alto Adige</p>
+        </footer>
 </body>
 </html>
+
